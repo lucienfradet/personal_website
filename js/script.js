d7d2dfa1ee00941f60ae3ad404b8454f5a2b3f2c
lucienfradet <lucien.fradet@hotmail.com>
2024-03-05 16:20:32 -0500

    main page finished. added gitignore for video files

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ $(document).ready(function() {
       // If window width is smaller than or equal to 991
       $(".navbar-icon").show();
       $(".navbar-desktop-menu").hide();
+      $(".buttom-social").hide();
     } else {
       mobileFlag = false;
       // If window width is greater than 991
       $(".navbar-icon").hide();
       $(".navbar-desktop-menu").show();
+      $(".buttom-social").show();
     }
   }
 
@@ -22,22 +24,27 @@ $(document).ready(function() {
   // Check on window resize
   $(window).resize(checkScreenSize);
 
-  // Function to handle scroll event
-  $(window).scroll(function() {
-    // Get the current scroll position
+  let lastScrollTop = 0;
+
+  function checkScrollPosition() {
     var scrollPosition = $(this).scrollTop();
 
-    if (!mobileFlag) {
-      // Check if the scroll position is greater than 20 pixels
-      if (scrollPosition > 30) {
-        // If true, transition navbar to the left
-        $(".navbar").css("transform", "translateY(-100%)");
-      } else {
-        // If false, reset navbar position
-        $(".navbar").css("transform", "translateY(0)");
-      }
+    // Check if the current scroll position is greater than the last scroll position
+    if (scrollPosition > lastScrollTop){
+      // Scrolling down
+      $(".navbar").css("transform", "translateY(-100%)");
+    } else {
+      // Scrolling up
+      $(".navbar").css("transform", "translateY(0)");
     }
-  });
+
+    // Update the last scroll position
+    lastScrollTop = scrollPosition <= 0 ? 0 : scrollPosition;
+  }
+
+  checkScrollPosition();
+
+  $(window).scroll(checkScrollPosition);
 
   // navbar mobile
   const navbarIconUrl = '../data/icons/navbar_icon.svg';
@@ -88,4 +95,118 @@ $(document).ready(function() {
     $('.navbar').toggleClass("active");
     $('.navbar-content').toggleClass("active");
   });
+
+  $(".media-container").hover(
+    function() {
+      let $video = $(this).find(".video"); // Find the video element
+      if ($video.length) { // Check if the video element exists
+        $video.show(); // Show the video
+        $(this).find(".image").css('opacity', 0); // Hide the image
+        $video.get(0).play(); // Play the video
+      }
+    },
+    function() {
+      let $video = $(this).find(".video"); // Find the video element
+      if ($video.length) { // Check if the video element exists
+        $video.hide(); // Hide the video
+        $(this).find(".image").css('opacity', 1); // Show the image
+        $video.get(0).pause(); // Pause the video
+      }
+    }
+  );
+
+  let images = $('.multiple-images img');
+  console.log(images);
+  let currentIndex = 0;
+  let previousIndex = 0;
+
+  function showImage(index) {
+    images.eq(previousIndex).css('opacity', 0);
+    images.eq(index).css('opacity', 1);
+  }
+
+  function nextImage() {
+    previousIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  // Initial setup
+  showImage(0);
+
+  // Set interval to switch images every 5 seconds
+  setInterval(nextImage, 5000); // Adjust the interval time as needed
+
+  // ******* adjust for mobile ********
+
+  // store contentElements
+  let contentLeft = [];
+  let contentRight = [];
+  let contentCenter = [];
+  let contentId = [];
+
+  $('.content.content-left').each(function() {
+    contentLeft.push($(this));
+  })
+  $('.content.content-right').each(function() {
+    contentRight.push($(this));
+  })
+  $('.content.content-center').each(function() {
+    contentCenter.push($(this));
+  })
+  $('.media-container').each(function() {
+    let id = $(this).attr('id');
+    if (id) {
+      contentId.push({ id: id, element: $(this) })
+    }
+  })
+
+  function addContentClass() {
+    for (const e of contentLeft) {
+      e.addClass('content-left')
+    }
+    for (const e of contentRight) {
+      e.addClass('content-right')
+    }
+    for (const e of contentCenter) {
+      e.addClass('content-center')
+    }
+    for (const e of contentId) {
+      e.element.attr('id', e.id);
+    }
+  }
+
+  function removeContentClass() {
+    for (const e of contentLeft) {
+      e.removeClass('content-left')
+    }
+    for (const e of contentRight) {
+      e.removeClass('content-right')
+    }
+    for (const e of contentCenter) {
+      e.removeClass('content-center')
+    }
+    for (const e of contentId) {
+      e.element.removeAttr('id');
+    }
+  }
+
+  function toggleMobileDisplay() {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth < 479) {
+      $('.pill-container').hide(); // Hide the pill container if screen width is less than 479
+      removeContentClass();
+    } else {
+      $('.pill-container').show(); // Show the pill container if screen width is 479 or greater
+      addContentClass();
+    }
+  }
+
+  // Initial call to set the initial state based on screen width
+  toggleMobileDisplay();
+
+  // Event listener to toggle pill container on window resize
+  $(window).resize(toggleMobileDisplay);
+
 })
